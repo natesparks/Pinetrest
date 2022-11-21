8f52a63c8ba5c2f93353e06dbc67acf338adc8a3
Neel Tangella <neel.tangella20@gmail.com>
2022-11-20 21:50:55 -0800

    added favorites to jdbc and sql

--- src/main/java/JDBC.java
+++ src/main/java/JDBC.java
@@ -11,64 +11,6 @@ public class JDBC {
 	private static String SQL_PASSWORD = "password"; 
 	
 	
-//	public static User loginUser(String username, String password) {
-//		
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver"); 
-//		}catch(ClassNotFoundException e) {
-//			System.out.println("Error in loginUser" + e.getMessage()); 
-//		}
-//		
-//		Connection conn = null; 
-//		Statement st = null; 
-//		
-//		try {
-//			conn = DriverManager.getConnection(JDBC_URL+ SQL_PASSWORD); 
-//			st = conn.createStatement(); 
-//			
-//		}catch(SQLException e) {
-//			System.out.println("SQLException in loginUser" + e.getMessage()); 
-//		}finally {
-//			try {
-//				if(st != null) {st.close(); }
-//				if(conn != null) {conn.close(); }
-//			}catch(SQLException e) {
-//				System.out.println("Closing SQLException in loginUser" + e.getMessage()); 
-//			}
-//		}
-//		
-//		
-//		
-//	}
-//	
-//	public static int registerUser(String username, String password, String email) {
-//		
-//		try {
-//			Class.forName("com.mysql.cj.jdbc.Driver"); 
-//		}catch(ClassNotFoundException e) {
-//			System.out.println("Error in loginUser" + e.getMessage()); 
-//		}
-//		
-//		Connection conn = null; 
-//		Statement st = null; 
-//		
-//		try {
-//			conn = DriverManager.getConnection(JDBC_URL+ SQL_PASSWORD); 
-//			st = conn.createStatement(); 
-//			
-//		}catch(SQLException e) {
-//			System.out.println("SQLException in registerUser" + e.getMessage()); 
-//		}finally {
-//			try {
-//				if(st != null) {st.close(); }
-//				if(conn != null) {conn.close(); }
-//			}catch(SQLException e) {
-//				System.out.println("Closing SQLException in registerUser" + e.getMessage()); 
-//			}
-//		}
-//		
-//	}
-	
 	public static Vector<String> getCollection(String userID, String collectionName){
 		
 		
@@ -228,6 +170,74 @@ public class JDBC {
 		}
 		return true; 
 		
+	}
+	
+	public static boolean insertIntoFavorites(String userID, String img_path) {
+		
+		if(userID == null || img_path == null) {
+			System.out.println("Cannot insert null values");
+			return false; 
+		}
+		else if (userID.equals("") || img_path.equals("")) {
+			System.out.println("Cannot insert empty values");
+			return false; 
+		}
+		
+		Connection conn = null; 
+		Statement st = null; 
+		
+		try {
+			conn = DriverManager.getConnection(JDBC_URL+ SQL_PASSWORD); 
+			st = conn.createStatement(); 
+			String query = "INSERT INTO Favorites(userID, img_path) values("+userID+", "+img_path+")";  
+			st.execute(query); 
+		}catch(SQLException e) {
+			System.out.println("SQLException in insertIntoFavorites" + e.getMessage()); 
+			return false; 
+		}finally {
+			try {
+				if(st != null) {st.close(); }
+				if(conn != null) {conn.close(); }
+			}catch(SQLException e) {
+				System.out.println("Closing SQLException in insertIntoFavorites" + e.getMessage()); 
+				return false; 
+			}
+		}
+		return true; 
+	}
+	
+public static boolean deleteFromFavorites(String userID, String img_path) {
+		
+		if(userID == null || img_path == null) {
+			System.out.println("Cannot insert null values");
+			return false; 
+		}
+		else if (userID.equals("") || img_path.equals("")) {
+			System.out.println("Cannot insert empty values");
+			return false; 
+		}
+		
+		Connection conn = null; 
+		Statement st = null; 
+		
+		try {
+			conn = DriverManager.getConnection(JDBC_URL+ SQL_PASSWORD); 
+			st = conn.createStatement(); 
+			String query = "DELETE FROM Favorites WHERE userID=" + userID+" AND img_path=" + img_path;  
+			st.execute(query); 
+		}catch(SQLException e) {
+			System.out.println("SQLException in insertIntoFavorites" + e.getMessage()); 
+			return false; 
+		}finally {
+			try {
+				if(st != null) {st.close(); }
+				if(conn != null) {conn.close(); }
+			}catch(SQLException e) {
+				System.out.println("Closing SQLException in insertIntoFavorites" + e.getMessage()); 
+				return false; 
+			}
+		}
+		return true; 
 	}
 	
 
---
+++ src/sql-scripts/create-favorites.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `Pinetrest`.`Favorites` (
+  `userID` VARCHAR(45) NOT NULL,
+  `img_path` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userID`));
